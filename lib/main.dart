eb4754e5c6e92acf66d4576c63b65428d5151238
LuisSiliezar <lsiliezarjr@gmail.com>
2022-07-12 09:09:18 -0600

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NETFLIX UI',
+      title: 'NETFLIX UI ',
       theme: ThemeData(
         // This is the theme of your application.
         //
